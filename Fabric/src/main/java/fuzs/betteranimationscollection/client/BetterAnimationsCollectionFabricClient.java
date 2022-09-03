220c0f85e639e0a4a09a80ff41247d2dcecb9109
Fuzss <28218241+Fuzss@users.noreply.github.com>
2022-09-03 17:18:24 +0200

    Update BetterAnimationsCollectionFabricClient.java

--- Fabric/src/main/java/fuzs/betteranimationscollection/client/BetterAnimationsCollectionFabricClient.java
+++ Fabric/src/main/java/fuzs/betteranimationscollection/client/BetterAnimationsCollectionFabricClient.java
@@ -1,6 +1,8 @@
 package fuzs.betteranimationscollection.client;
 
 import fuzs.betteranimationscollection.BetterAnimationsCollection;
+import fuzs.betteranimationscollection.api.event.entity.living.LivingEvents;
+import fuzs.betteranimationscollection.client.handler.RemoteSoundHandler;
 import fuzs.puzzleslib.client.core.ClientCoreServices;
 import net.fabricmc.api.ClientModInitializer;
 
@@ -9,5 +11,10 @@ public class BetterAnimationsCollectionFabricClient implements ClientModInitiali
     @Override
     public void onInitializeClient() {
         ClientCoreServices.FACTORIES.clientModConstructor(BetterAnimationsCollection.MOD_ID).accept(new BetterAnimationsCollectionClient());
+        registerHandlers();
+    }
+
+    private static void registerHandlers() {
+        LivingEvents.TICK.register(RemoteSoundHandler.INSTANCE::onLivingTick);
     }
 }
